5f3d2f6a7ee0a29ab323f527c900f6779c617a35
robertdippolito <robert.e.dippolito@gmail.com>
2018-07-10 22:18:26 -0400

    completed todo list app with Flip Move extension

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import TodoItems from './TodoItems.js';
+import './TodoList.css';
 
 class TodoList extends Component {
   constructor(props) {
@@ -8,6 +10,7 @@ class TodoList extends Component {
     };
 
     this.addItem = this.addItem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
   }
 
   addItem(e) {
@@ -29,6 +32,15 @@ class TodoList extends Component {
     e.preventDefault();
   }
 
+  deleteItem(key) {
+    var filteredItems = this.state.items.filter(function(item){
+      return (item.key !== key)
+    });
+    this.setState({
+      items: filteredItems
+    });
+  }
+
   render() {
     return (
       <div className="todoListMain">
@@ -38,6 +50,8 @@ class TodoList extends Component {
             <button type="submit">Add</button>
           </form>
         </div>
+        <TodoItems entries={ this.state.items }
+                    delete={ this.deleteItem }/>
       </div>
     );
   }
